9881361eae05403cbbfa34f86495da792647fbd6
Juraj Onuska <jurajonuska@gmail.com>
2024-08-04 22:03:45 +0200

    extended chat types

--- api/openai/models/chat.py
+++ api/openai/models/chat.py
@@ -16,16 +16,65 @@ class ChatCompletionRequestMessage(BaseModel):
     ]
 
 
+class AssistantsApiResponseFormat(BaseModel):
+    type: Literal["text", "json_object"] = "text"
+
+
+class ChatCompletionStreamOptions(BaseModel):
+    include_usage: bool | None = None
+
+
+class FunctionObject(BaseModel):
+    description: str | None = None
+    name: str
+    parameters: dict | None = None
+
+
+class ChatCompletionTool(BaseModel):
+    type: Literal["function"] = "function"
+    function: FunctionObject
+
+
+class ChatCompletionNamedToolChoice(BaseModel):
+    name: str
+
+
+class ChatCompletionToolChoiceOption(BaseModel):
+    type: Literal["function"] = "function"
+    function: ChatCompletionNamedToolChoice
+
+
 class CreateChatCompletionRequest(BaseModel):
     model_config = ConfigDict(extra="allow")
 
     model: str
     messages: list[ChatCompletionRequestMessage]
+
     stream: bool | None = True
     user: str | None = None
+    service_tier: Literal["auto", "default"] | None = None
+
+    frequency_penalty: float | None = 0.0
+    presence_penalty: float | None = 0.0
+
+    logit_bias: dict[str, float] | None = None
+    logprobs: bool | None = False
+    top_logprobs: int | None = 0
+
+    n: int | None = 1
+    max_tokens: int | None = None
+
+    response_format: AssistantsApiResponseFormat | None = None
+    stream_options: ChatCompletionStreamOptions | None = None
+
     temperature: float | None = 1.0
     top_p: float | None = 1.0
     seed: int | None = None
+    stop: str | list[str] | None = None
+
+    tools: list[ChatCompletionTool] | None = None
+    tool_choice: str | ChatCompletionToolChoiceOption | None = None
+    parallel_tool_calls: bool | None = True
 
 
 class CompletionUsage(BaseModel):
@@ -104,6 +153,8 @@ class CreateChatCompletionStreamResponse(BaseModel):
     choices: list[CreateChatCompletionStreamChoiceResponse]
     created: int
     model: str
+    service_tier: Literal["scale", "default"] | None = None
+    system_fingerprint: str
     object_: Literal["chat.completion.chunk"] = Field(
         default="chat.completion.chunk",
         alias="object",
